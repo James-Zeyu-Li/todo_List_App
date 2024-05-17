6d995b57e7ceb8dc4462041e49eb694699610228
James_ZY_L <zeyujames@gmail.com>
2024-05-17 00:27:41 -0700

    Update main to be the main entrance for app

--- main.py
+++ main.py
@@ -2,25 +2,32 @@
 This is a practice project to create a to do list application using python.
 """
 
-from view.todo_GUI import TodoGUI
+import sys
+from PyQt5.QtWidgets import QApplication
 from controller.todo_controller import TodoController
 from model.todo_model import Todo
+from view.todo_GUI import TodoGUI
 
 
 def main():
-
+    """
+    Main function to initialize and run the todo list application.
+    It sets up the MVC components and handles potential exceptions.
+    """
     try:
+        app = QApplication(sys.argv)
         model = Todo()
-        controller = TodoController(model, None)
+        controller = TodoController(model)
         gui = TodoGUI(controller)
-        controller.view = gui  # 设置视图引用
-        controller.run()
+        controller.view = gui
+        gui.run()
+        sys.exit(app.exec_())
     except ValueError as ex:
-        print(type(ex), ex)
+        print(f"ValueError occurred: {ex}")
     except TypeError as ex:
-        print(type(ex), ex)
+        print(f"TypeError occurred: {ex}")
     except IndexError as ex:
-        print(type(ex), ex)
+        print(f"IndexError occurred: {ex}")
 
 
 if __name__ == "__main__":
